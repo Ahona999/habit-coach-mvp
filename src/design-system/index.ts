12aa81f1c7dc64da4ffac1632e074977aa814191
Ahoona-Kar <ahonauno@gmail.com>
2025-12-20 07:46:40 +0530

    chore: expose design system theme via index

--- src/design-system/index.ts
+++ src/design-system/index.ts
@@ -1,4 +1,21 @@
-export * from "./colors";
-export * from "./typography";
-export * from "./spacing";
-export * from "./breakpoints";
+import { colors } from './colors';
+import { spacing } from './spacing';
+import { typography } from './typography';
+import { breakpoints } from './breakpoints';
+
+export type ThemeMode = 'light' | 'dark';
+
+export const getTheme = (mode: ThemeMode = 'light') => ({
+  colors: colors[mode],
+  spacing,
+  typography,
+  breakpoints,
+});
+
+export const designSystem = {
+  colors,
+  spacing,
+  typography,
+  breakpoints,
+};
+
