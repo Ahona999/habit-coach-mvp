14aac626c64030b55a408dfebf0be5e8f92d80b0
Ahoona-Kar <ahonauno@gmail.com>
2025-12-21 23:49:24 +0530

    changes made in auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,9 +1,11 @@
-await supabase.auth.signInWithOtp({
+const { data, error } = await supabase.auth.signInWithOtp({
   email,
   options: {
     emailRedirectTo: window.location.origin,
   },
 });
+
+console.log({ data, error });
 
 
 
